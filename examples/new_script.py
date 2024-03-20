53f193f68d18b65cea1cffe55684a16997349dd0
ezedalessandro21 <ezequiel.dalessandro@nasini.com.ar>
2024-03-20 14:06:06 -0300

    selenium

--- examples/new_script.py
+++ examples/new_script.py
@@ -1,28 +1,28 @@
+import logging
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+# Configurar logging
+logging.basicConfig(filename='log.txt', level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
+
 class SimpleGoogleTester:
     def __init__(self, url: str, headless: bool = True):
-        # Configurar las opciones de Chrome
         chrome_options = Options()
         if headless:
             chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
-
-        # Inicializar el WebDriver con las opciones definidas
+        
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get(url)
+        logging.info("Google ha sido abierto en el navegador headless.")
 
     def close(self):
-        # Cerrar el navegador
         if self.driver:
             self.driver.quit()
+            logging.info("El navegador se ha cerrado correctamente.")
 
 if __name__ == "__main__":
-    # Crear una instancia de SimpleGoogleTester y abrir Google
     tester = SimpleGoogleTester("https://www.google.com", headless=True)
-    print("Google ha sido abierto en el navegador headless.")
-    # Cerrar el navegador
     tester.close()
-    print("El navegador se ha cerrado correctamente.")
